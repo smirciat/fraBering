045fad171265195fc555dca485184cbaf36ef3f8
Andy Smircich <smirciat@hotmail.com>
2025-04-27 06:49:30 -0800

    final fuel burn

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -110,7 +110,7 @@ class NavbarController {
   }
   
   stoppedFunction(){
-    let version='47';
+    let version='48';
     this.http.post('/api/todaysFlights/stopped'+version).then(res=>{
       window.localStorage.setItem('stopped','true');
       console.log('Stopped Value ('+version+') is '+res.data.stopped);
