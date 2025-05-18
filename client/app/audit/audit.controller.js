97f5a3a01a7928a7f7e8e54932b4a8247ac60ccc
Andy Smircich <smirciat@hotmail.com>
2025-05-18 14:46:52 -0800

    custom audits

--- client/app/audit/audit.controller.js
+++ client/app/audit/audit.controller.js
@@ -8,7 +8,7 @@ class AuditComponent {
     this.scope=$scope;
     this.timeout=$timeout;
     this.Auth=Auth;
-    this.startDate=new Date();
+    this.startDate=new Date(new Date().setDate(new Date().getDate()-1));
     this.startDate.setHours(10);
     this.startDateStringFormatted=this.startDate.toLocaleDateString();
     this.endDate=new Date();
@@ -16,14 +16,26 @@ class AuditComponent {
     this.endDateStringFormatted=this.endDate.toLocaleDateString();
     this.pilots=[];
     this.pfrs=[];
+    this.flights=[];
+    this.selectedItems = { multipleSelect: [] };
     this.pfrQuery={collection:'flights',limit:3000,parameter:'date',operator:'>=',value:this.startDate,
         parameter2:'date',operator2:'<=',value2:this.endDate,timestampBoolean:true};
   }
   
   $onInit(){
     this.setPilots();
-    
-    
+    this.setFlights();
+    this.scope.$watch('audit.selectedItems.multipleSelect', (newValues, oldValues)=> {
+      if (newValues !== oldValues && oldValues) {
+        this.flightKeys.forEach(obj=>{obj.selected=false});
+        console.log(newValues)
+        newValues.forEach(selected=>{
+          let index=this.flightKeys.findIndex(e=>e.key===selected.key&&e.collection===selected.collection);
+          if (index>-1) this.flightKeys[index].selected=true;
+        });
+        window.localStorage.setItem('flightKeys',JSON.stringify(this.flightKeys));
+      }
+    }, true);
   }
   
   setPilots(){
@@ -34,6 +46,57 @@ class AuditComponent {
       });
       this.pilots.unshift({displayName:"None"});
     });
+  }
+  
+  async setFlights(){
+    this.flightKeys=[];
+    let temp=window.localStorage.getItem('flightKeys');
+    if (temp) this.flightKeys=JSON.parse(temp);
+    let date=new Date(this.startDate).toLocaleDateString();
+    let res=await this.http.post('/api/todaysFlights/dayFlights',{dateString:date});
+    this.flights=res.data;
+    for (let flight of this.flights){
+      for (let key in flight){
+        let index=this.flightKeys.findIndex(e => e.key === key && e.collection === 'flights');
+        let info=typeof flight[key];
+        let combo=key + ' - from flights, example: ' + flight[key];
+        let obj={key:key,
+              info:info,collection:'flights',
+              combo:combo};
+        if (info==='string'||info==='number'||info==='boolean'||(info==='object'&&flight[key] instanceof Date)){
+          if (info==='object') info='date';
+          if (index===-1) this.flightKeys.push(obj);
+          else {
+            obj.selected=this.flightKeys[index].selected;
+            this.flightKeys[index]=obj;
+          }
+        }
+      }
+      for (let key in flight.pfr){
+        let index=this.flightKeys.findIndex(e => e.key === key && e.collection === 'pfrs');
+        let info=typeof flight.pfr[key];
+        let e=flight.pfr[key];
+        if (key==='date') e=flight.pfr[key]._seconds;
+        let combo=key + ' - from pfrs, example: ' + e;
+        let obj={key:key,
+              info:info,collection:'pfrs',
+              combo:combo};
+        if (info==='string'||info==='number'||info==='boolean'||(info==='object'&&(flight.pfr[key] instanceof Date||flight.pfr[key]._seconds))){
+          if (info==='object') obj.info='date';
+          if (index===-1) this.flightKeys.push(obj);
+          else {
+            obj.selected=this.flightKeys[index].selected;
+            this.flightKeys[index]=obj;
+          }
+        }
+      }
+    }
+    console.log(this.flightKeys);
+    window.localStorage.setItem('flightKeys',JSON.stringify(this.flightKeys));
+    this.flightKeys.forEach(obj=>{
+      if (obj.selected) this.selectedItems.multipleSelect.push(obj);
+    });
+    
   }
   
   setPfrs(){
@@ -59,8 +122,77 @@ class AuditComponent {
     this.upDate(element);
   }
   
-  createCSVReleases(){
+  daysBetween(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return daysDiff;
+  }
+  
+  async createCSVCustom(){
     this.complete=false;
+    this.complete2=false;
+    this.complete3=false;
+    this.spinner=true;
+    this.csv="";
+    let keys=[];
+    this.flightKeys.forEach((obj,i)=>{
+      if (obj.selected) {
+        keys.push(obj);
+        this.csv+=obj.key+',';
+      }
+      if (i===this.flightKeys.length-1) {
+        this.csv=this.csv.slice(0, -1);
+        this.csv+="\r\n";
+      }
+    });
+    let startInt=new Date(this.startDate).getDate();
+    let endInt=startInt+this.daysBetween(new Date(this.startDate),new Date(this.endDate))-1;
+    for (let x=startInt;x<=endInt;x++) {
+      let date=new Date(this.startDate);
+      date.setDate(x);
+      let res=await this.http.post('/api/todaysFlights/dayFlights',{dateString:date.toLocaleDateString()});
+      
+      let flights=res.data;
+      //if (this.pilot) flights=flights.filter(p=>{return p.pilot===this.pilot.displayName});
+      flights.sort((a,b)=>{
+        return a.pilot.localeCompare(b.pilot);
+      });
+      for(const flight of flights){
+        keys.forEach((obj,i)=>{
+          if (obj.collection==='flights') {
+            if (flight[obj.key]&&typeof flight[obj.key]==='string') flight[obj.key]=flight[obj.key].replaceAll(',','-');
+            this.csv+=flight[obj.key]+',';
+          }
+          if (obj.collection==='pfrs'){
+            if (flight.pfr) {
+              if (flight.pfr[obj.key]&&typeof flight.pfr[obj.key]==='string') flight.pfr[obj.key]=flight.pfr[obj.key].replaceAll(',','-');
+              if (obj.info==='date') {
+                if (flight.pfr[obj.key]) this.csv+=flight.pfr[obj.key]._seconds+',';
+                else this.csv+=',';
+              }
+              else this.csv+=flight.pfr[obj.key]+',';
+            }
+            else this.csv+=',';
+          }
+          if (i===keys.length-1) {
+            this.csv=this.csv.slice(0, -1);
+            this.csv+="\r\n";
+          }
+        });
+      }
+    }
+    this.spinner=false;
+    console.log(this.csv);
+    this.complete3=true;
+    let blob = new Blob([ this.csv ], { type : 'text/plain' });
+    this.url = (window.URL || window.webkitURL).createObjectURL( blob );
+  }
+  
+  createCSVReleases(){
+    this.customAudit=false;
+    this.complete=false;
+    this.complete2=false;
+    this.complete3=false;
     this.spinner=true;
     let date=new Date(this.startDate).toLocaleDateString();
     return this.http.post('/api/todaysFlights/dayFlights',{dateString:date}).then(res=>{
@@ -89,14 +221,17 @@ class AuditComponent {
       }
       this.spinner=false;
       console.log(this.csv);
-      this.complete=true;
+      this.complete2=true;
       let blob = new Blob([ this.csv ], { type : 'text/plain' });
       this.url = (window.URL || window.webkitURL).createObjectURL( blob );
     });
   }
   
   createCSV(){
+    this.customAudit=true;
     this.complete=false;
+    this.complete2=false;
+    this.complete3=false;
     this.spinner=true;
     return this.http.post('/api/airplanes/firebaseQuery',this.pfrQuery).then(res=>{
       this.csv="PILOT,DATE,FLIGHTNUM,AIRCRAFT,AIRCRAFT TYPE,OWE,FUEL,LOAD AVAILABLE,MGTOW,CG,OW,TOW\r\n";
