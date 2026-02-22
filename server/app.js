7729e7817ee93d5e7f78e05dffe27af09b7a96a7
Andy Smircich <smirciat@hotmail.com>
2026-02-21 15:26:04 -0900

    remove rate limiter

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
@@ -69,7 +69,7 @@ app.use(
     frameguard: false, 
   })
 );
-app.use(limiter);
+//app.use(limiter);
 //var server = http.createServer(app);
 var server = https.createServer(credentials,app);
 var socketio = require('socket.io')(server, {
