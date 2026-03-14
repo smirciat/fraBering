f0fe5451dddfcbd122b0e5437ae81b866e268529
Andy Smircich <smirciat@hotmail.com>
2026-03-14 08:53:20 -0800

    airport-blue

--- client/components/modal/modal.service.js
+++ client/components/modal/modal.service.js
@@ -217,6 +217,7 @@ angular.module('workspaceApp')
                   return targetTime >= now;
                 },
                 ocRequired=function(color){
+                  if (color==='airport-blue') return true;
                   if (colors.indexOf(color)>3) return true;
                   if (flight.pfr&&flight.pfr.legArray[0].fuel<flight.equipment.minFuel) return true;
                   if (flight.knownIce&&flight.equipment.name==="Caravan") return true;
@@ -413,7 +414,7 @@ angular.module('workspaceApp')
                   if (moreThanOneHour()) string+='- The flight is within one hour of scheduled departure,\r\n';
                   if (noPfr()) string+='- The captain has successfully created a PFR and entered fuel quantity,\r\n';
                   if (!isAdmin) string+='- You are logged in as an OC Manager or Dispatcher';
-                  if (ocRequired(flight.color)) string+='- Flight color is NOT orange or red, and the FIKI box is NOT checked (Caravans)\r\n';
+                  if (ocRequired(flight.color)) string+='- Flight color is NOT orange, red, or blue, and the FIKI box is NOT checked (Caravans)\r\n';
                   if (string.length<55) string+='All criteria for signing appear to have been met.  If you can`t sign, something unexpected has happened.';
                   window.alert(string);
                 },
@@ -422,7 +423,7 @@ angular.module('workspaceApp')
                   if (moreThanOneHour()) string+='- The flight is within one hour of scheduled departure,\r\n';
                   if (noPfr()) string+='- The captain has successfully created a PFR and entered fuel quantity,\r\n';
                   if (!isSuperAdmin) string+='- You are logged in as an OC Manager';
-                  if (!ocRequired(flight.color)) string+='- Flight color is orange or red, or the FIKI box is checked (Caravans)\r\n';
+                  if (!ocRequired(flight.color)) string+='- Flight color is orange, red, or blue, or the FIKI box is checked (Caravans)\r\n';
                   if (string.length<55) string+='All criteria for signing appear to have been met.  If you can`t sign, something unexpected has happened.';
                   window.alert(string);
                 },
