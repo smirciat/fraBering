ddb43368379b2025b6ca745336c65d5c4c68807b
Andy Smircich <smirciat@hotmail.com>
2026-08-06 19:47:39 -0800

    stopped125

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -130,7 +130,7 @@ class NavbarController {
   }
   
   stoppedFunction(){
-    let version='124';
+    let version='125';
     this.http.post('/api/todaysFlights/stopped'+version).then(res=>{
       window.localStorage.setItem('stopped','true');
       console.log('Stopped Value ('+version+') is '+res.data.stopped);
